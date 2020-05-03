b55537f0730dfeb1ac41463fba2f45cd884a0a75
kyubuns <kyubuns@gmail.com>
2020-05-04 01:40:02 +0900

    feat: チェック用にDemoシーンセットアップ

---
+++ Assets/Plugins/AppIconChanger/Demo/Demo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace AppIconChanger.Demo
+{
+    public class Demo : MonoBehaviour
+    {
+        private string supportsAlternateIcons = "(Empty)";
+        private string alternateIconName = "(Empty)";
+
+        public void OnGUI()
+        {
+            GUI.TextArea(new Rect(10, 10, 300, 50), $"supportsAlternateIcons = {supportsAlternateIcons}");
+            GUI.TextArea(new Rect(10, 60, 300, 50), $"alternateIconName = {alternateIconName}");
+
+            if (GUI.Button(new Rect(10, 120, 300, 100), "Get SupportsAlternateIcons"))
+            {
+                supportsAlternateIcons = iOS.SupportsAlternateIcons.ToString();
+            }
+
+            if (GUI.Button(new Rect(10, 220, 300, 100), "Get AlternateIconName"))
+            {
+                alternateIconName = iOS.AlternateIconName.ToString();
+            }
+
+            if (GUI.Button(new Rect(10, 320, 300, 100), "Change Icon Red"))
+            {
+                iOS.SetAlternateIconName("demo_red");
+            }
+
+            if (GUI.Button(new Rect(10, 420, 300, 100), "Change Icon Green"))
+            {
+                iOS.SetAlternateIconName("demo_green");
+            }
+
+            if (GUI.Button(new Rect(10, 520, 300, 100), "Change Icon Blue"))
+            {
+                iOS.SetAlternateIconName("demo_blue");
+            }
+        }
+    }
+}
---
+++ Assets/Plugins/AppIconChanger/Runtime/iOS.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AppIconChanger
+{
+    public static class iOS
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern bool _SupportsAlternateIcons();
+
+        [DllImport("__Internal")]
+        private static extern string _AlternateIconName();
+
+        [DllImport("__Internal")]
+        private static extern void _SetAlternateIconName(string iconName);
+#else
+        private static bool _SupportsAlternateIcons()
+        {
+            UnityEngine.Debug.Log("SupportsAlternateIcons is not supported on this device.");
+            return false;
+        }
+
+        private static string _AlternateIconName()
+        {
+            UnityEngine.Debug.Log("AlternateIconName is not supported on this device.");
+            return string.Empty;
+        }
+
+        private static void _SetAlternateIconName(string iconName)
+        {
+            UnityEngine.Debug.Log("SetAlternateIconName is not supported on this device.");
+        }
+#endif
+
+        public static bool SupportsAlternateIcons => _SupportsAlternateIcons();
+
+        public static string AlternateIconName => _AlternateIconName();
+
+        public static void SetAlternateIconName(string iconName)
+        {
+            _SetAlternateIconName(iconName);
+        }
+    }
+}
