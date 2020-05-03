2d8a617911fa77cf122d76f1729c8a5f5f3c0f22
kyubuns <kyubuns@gmail.com>
2020-05-04 01:57:07 +0900

    feat: iOS

---
+++ Assets/Plugins/AppIconChanger/Runtime/appiconchanger.mm
@@ -0,0 +1,39 @@
+#if ! __has_feature(objc_arc)
+#error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag
+#endif
+
+NSString* AppIconChanger_CreateNSString(const char* string)
+{
+	return [NSString stringWithUTF8String: string ? string : ""];
+}
+
+char* AppIconChanger_MakeStringCopy(const char* string)
+{
+	if (string == NULL) return NULL;
+	char* res = (char*)malloc(strlen(string) + 1);
+	strcpy(res, string);
+	return res;
+}
+
+extern "C"
+{
+	bool _SupportsAlternateIcons()
+	{
+        return [[UIApplication sharedApplication] supportsAlternateIcons];
+	}
+
+	char *_AlternateIconName()
+	{
+		NSString *string = [[UIApplication sharedApplication] alternateIconName];
+		return AppIconChanger_MakeStringCopy([string UTF8String]);
+	}
+
+	void _SetAlternateIconName(const char* iconName)
+	{
+		NSString *nsstringText = AppIconChanger_CreateNSString(iconName);
+        [[UIApplication sharedApplication] setAlternateIconName:nsstringText completionHandler:^(NSError * _Nullable error) {
+            NSLog(@"_SetAlternateIconName.Error %@", error);
+        }];
+	}
+}
+
