d90eb1d9c2d2fd1a4bac023d2de9e363fb99a77b
kyubuns <kyubuns@gmail.com>
2020-05-04 00:12:30 +0900

    feat: AlternateIcons

---
+++ Assets/Plugins/AppIconChanger/Editor/AlternateIcons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace AppIconChanger.Editor
+{
+    [Serializable]
+    [CreateAssetMenu]
+    public class AlternateIcons : ScriptableObject
+    {
+        public Icon[] icons;
+    }
+
+    [Serializable]
+    public class Icon
+    {
+        public string name;
+        public Texture iPhone180Px; // 180x180px
+        public Texture iPhone120Px; // 120x120px
+        public Texture iPad167Px; // 167x167px
+        public Texture iPad152Px; // 152x152px
+        public Texture iPad76Px; // 76x76px
+    }
+}
