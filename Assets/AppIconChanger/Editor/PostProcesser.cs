cc12cb5b150c5a04676986b643c93f894e0bcf2b
kyubuns <kyubuns@gmail.com>
2022-08-21 16:18:35 +0900

    fix: build error when build target is not iOS

--- Assets/AppIconChanger/Editor/PostProcesser.cs
+++ Assets/AppIconChanger/Editor/PostProcesser.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿#if UNITY_IOS
+using System.IO;
 using System.Linq;
 using System.Text;
 using UnityEditor;
@@ -229,3 +230,4 @@ namespace AppIconChanger.Editor
 ";
 	}
 }
+#endif
