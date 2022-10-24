fcc0f1359eced26dee59b5c66b6d754c32c0df25
Christophe SAUVEUR <christophe.sauveur@altshift.fr>
2022-10-24 17:43:35 +0200

    New feature: Create scriptable objects from selection

--- Assets/AppIconChanger/Editor/AlternateIcon.cs
+++ Assets/AppIconChanger/Editor/AlternateIcon.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using UnityEngine;
+using UnityEditor;
 // ReSharper disable InconsistentNaming
 
 namespace AppIconChanger.Editor
@@ -30,6 +32,52 @@ namespace AppIconChanger.Editor
         public Texture2D iPadApp152px;
         public Texture2D iPadProApp167px;
         public Texture2D appStore1024px;
+
+        [MenuItem("Assets/Create/AppIconChanger/AlternateIcon from Selection...")]
+        private static void CreateAlternateIconFromSelection() {
+            Texture2D[] selectedTexture2Ds = GetFilteredSelection();
+            foreach (Texture2D tex in selectedTexture2Ds) {
+                string assetPath = AssetDatabase.GetAssetPath(tex);
+                if (string.IsNullOrEmpty(assetPath)) {
+                    continue;
+                }
+
+                string directory = Path.GetDirectoryName(assetPath);
+                string filename = Path.GetFileNameWithoutExtension(assetPath);
+                string basePath = Path.Combine(directory, $"{filename}.asset");
+
+                bool overwrite = true;
+                if (File.Exists(basePath)) {
+                    int result = EditorUtility.DisplayDialogComplex("Warning", $"A file already exists at\n\n{basePath}\n\nWhat do you want to do?", "Overwrite", "Generate Unique Name", "Skip");
+                    if (result == 2) {
+                        continue;
+                    }
+                    overwrite = (result == 0);
+                }
+                
+                if (!overwrite) {
+                    basePath = AssetDatabase.GenerateUniqueAssetPath(basePath);
+                }
+
+                AlternateIcon icon = ScriptableObject.CreateInstance<AlternateIcon>();
+                icon.iconName = filename;
+                icon.source = tex;
+                if (overwrite) {
+                    AssetDatabase.DeleteAsset(basePath);
+                }
+                AssetDatabase.CreateAsset(icon, basePath);
+            }
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+        }
+
+        [MenuItem("Assets/Create/AppIconChanger/AlternateIcon from Selection...", isValidateFunction: true)]
+        private static bool CanCreateAlternateIconFromSelection() {
+            return GetFilteredSelection().Length > 0;
+        }
+
+        private static Texture2D[] GetFilteredSelection() {
+            return Selection.GetFiltered<Texture2D>(SelectionMode.Assets | SelectionMode.TopLevel);
+        }
     }
 
     public enum AlternateIconType
